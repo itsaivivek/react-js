a5152545f1d3a5ca0179063844809782743af60c
itsaivivek <er.vivek.social@gmail.com>
2026-07-19 10:36:35 +0545

    Tailwind css with react

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-zinc-800 h-screen w-full text-white p-6 leading-loose'>
+      <h2 className='text-4xl text-cyan-400'>About Me</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit magni voluptatem accusantium.</p>
+      <button className='bg-orange-600 px-5 py-2 rounded-full'>See More</button>
+    </div>
+  )
+}
+
+export default App
