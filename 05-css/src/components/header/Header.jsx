a0143b186c869352d26a6d55fa8a63f1b3bb6cc1
itsaivivek <er.vivek.social@gmail.com>
2026-07-19 10:28:29 +0545

    Best way to configure css in react App

---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h2>This is Header</h2>
+      <button className={styles.btn}>Save</button>
+    </div>
+  )
+}
+
+export default Header
